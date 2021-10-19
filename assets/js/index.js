5f8388854e60421144c7096af16bb4750ac8ba93
Manshur Notebook <manshurmuhammad2@gmail.com>
2021-10-19 21:47:36 +0700

    add icon and services section

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,12 @@
+// global variable
+let navbar = document.getElementById('navbar');
+
+// scroll navbar
+window.addEventListener("scroll", function(){
+    navbar.classList.toggle("scroll", window.scrollY > 0);
+})
+
 function toggleActive() {
     document.getElementById("burger").classList.toggle("active");
-    document.getElementById("navbar").classList.toggle("active");
+    navbar.classList.toggle("active");
 }
